701f360ac4cb9dca905bafcae5256846729d5ee9
Sayan Das <121926159+sayandas2003@users.noreply.github.com>
2025-04-10 23:18:57 +0530

    rest incomplete

--- REST/index.js
+++ REST/index.js
@@ -6,14 +6,31 @@ const path = require("path");
 app.use(express.urlencoded( {extended: true }));
 
 app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "public"));
+app.set("views", path.join(__dirname, "views"));
 
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
 
 
-app.get("/", (req, res) => {
-    res.send("server working well");
+let posts = [
+    {
+        username : "apnacollege",
+        content : "I love coding!"
+    },
+    {
+        username : "shradhakhapra",
+        content : "Hard work is important to achieve success"
+    },
+    {
+        username : "rahulkumar",
+        content : "I got selected for my 1st internship!"
+    }
+]
+
+
+
+app.get("/posts", (req, res) => {
+    res.render("index.ejs", { posts });
 });
 
 
