d638dcd58062b0aa4abb128b276cea3b935791d4
Sayan Das <121926159+sayandas2003@users.noreply.github.com>
2025-04-13 23:27:23 +0530

    rest almost complete

--- REST/index.js
+++ REST/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const port = 8080;
 const path = require("path");
+const {v4 : uuidv4} = require('uuid');
 
 app.use(express.urlencoded( {extended: true }));
 
@@ -14,14 +15,17 @@ app.use(express.json());
 
 let posts = [
     {
+        id: uuidv4(),
         username : "apnacollege",
         content : "I love coding!"
     },
     {
+        id: uuidv4(),
         username : "shradhakhapra",
         content : "Hard work is important to achieve success"
     },
     {
+        id: uuidv4(),
         username : "rahulkumar",
         content : "I got selected for my 1st internship!"
     }
@@ -31,6 +35,41 @@ let posts = [
 
 app.get("/posts", (req, res) => {
     res.render("index.ejs", { posts });
+});
+
+app.get("/posts/new", (req, res) => {
+    res.render("new.ejs");
+});
+
+app.post("/posts", (req, res) => {
+    // console.log(req.body);
+    // as in post request the data is inside body and in get request data is in parameters
+    let {username, content} = req.body;
+    let id = uuidv4();
+    posts.push({id, username, content});
+    // res.send("post request working");
+    res.redirect("/posts");
+});
+
+app.get("/posts/:id", (req, res) => {
+    let {id} = req.params;
+    let post = posts.find((p) => id === p.id);
+    res.render("show.ejs", {post});
+});
+
+app.patch("/posts/:id", (req, res) => {
+    let {id} = req.params;
+    let newContent = req.body.content;
+    let post = posts.find((p) => id === p.id);
+    post.content = newContent;
+    console.log(post);
+    res.send("patch request working");
+});
+
+app.get("/posts/:id/edit", (req, res) => {
+    let {id} = req.params;
+    let post = posts.find((p) => id === p.id);
+    res.render("edit.ejs", {post});
 });
 
 
