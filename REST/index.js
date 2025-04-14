f2b8db2bb465121995d106d430e4b5809820f1c5
Sayan Das <121926159+sayandas2003@users.noreply.github.com>
2025-04-14 10:32:33 +0530

    rest completed

--- REST/index.js
+++ REST/index.js
@@ -3,8 +3,10 @@ const app = express();
 const port = 8080;
 const path = require("path");
 const {v4 : uuidv4} = require('uuid');
+const methodOverride = require("method-override");
 
 app.use(express.urlencoded( {extended: true }));
+app.use(methodOverride('_method'));
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
@@ -63,7 +65,7 @@ app.patch("/posts/:id", (req, res) => {
     let post = posts.find((p) => id === p.id);
     post.content = newContent;
     console.log(post);
-    res.send("patch request working");
+    res.redirect("/posts");
 });
 
 app.get("/posts/:id/edit", (req, res) => {
@@ -71,6 +73,12 @@ app.get("/posts/:id/edit", (req, res) => {
     let post = posts.find((p) => id === p.id);
     res.render("edit.ejs", {post});
 });
+
+app.delete("/posts/:id", (req, res) => {
+    let {id} = req.params;
+    posts = posts.filter((p) => id !== p.id);
+    res.redirect("/posts");
+})
 
 
 app.listen(port, ()=> {
