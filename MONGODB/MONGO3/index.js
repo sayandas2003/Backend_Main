2e9bfc0a2a09f910aaab1fb244aef0a538a9f23f
Sayan Das <121926159+sayandas2003@users.noreply.github.com>
2025-04-21 17:54:50 +0530

    whatsapp noob

---
+++ MONGODB/MONGO3/index.js
@@ -0,0 +1,83 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const path = require("path");
+const Chat = require("./models/chats.js");
+
+
+app.use(express.static(path.join(__dirname, "public")));
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+
+main()
+.then(() => {
+    console.log("connection successfull");
+})
+.catch(() => {
+    console.log("connection error");
+})
+
+async function main() {
+    await mongoose.connect("mongodb://127.0.0.1:27017/whatsapp");
+}
+
+
+
+//Index Route
+app.get("/chats", async (req, res) => {
+    let chats = await Chat.find();
+    // console.log(chats);
+    res.render("index.ejs", {chats});
+})
+
+// New Route
+app.get("/chats/new", (req, res) => {
+    res.render("new.ejs");
+})
+
+//Create Route
+app.post("/chats", (req, res) => {
+    let {from, to, msg} = req.body;
+    let newChat = new Chat({
+        from: from,
+        to: to,
+        msg: msg,
+        created_at: new Date()
+    })
+    newChat.save()
+    .then((res) => {
+        console.log(res);
+    })
+    .catch((err) => {
+        console.log(err);
+    })
+    res.redirect("/chats");
+})
+
+
+
+
+app.get("/", (req, res) => {
+    res.send("root is working");
+})
+app.listen(8080, () => {
+    console.log("server is listening on port 8080");
+})
+
+
+
+
+
+// let chat1 = new Chat({
+//     from: "neha",
+//     to: "priya",
+//     msg: "send me your exam sheets",
+//     created_at: new Date()
+// });
+
+// chat1.save()
+// .then((res) => {
+//     console.log(res);
+// })
