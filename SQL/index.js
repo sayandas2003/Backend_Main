9b00c97413258752a3631235baf316a1d68fd4a7
Sayan Das <121926159+sayandas2003@users.noreply.github.com>
2025-04-24 22:13:08 +0530

    mysql with node

---
+++ SQL/index.js
@@ -0,0 +1,33 @@
+const { faker } = require('@faker-js/faker');
+const mysql = require('mysql2');
+
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    database: 'delta_app',
+    password: 'sayandas2003'
+  });
+
+
+try {
+    connection.query("SHOW TABLES", (err, result) => {
+        if(err) throw err;
+        console.log(result);
+    });
+} catch (err) {
+    console.log(err);
+}
+
+
+
+let getRandomUser = () => {
+    return {
+      Id: faker.string.uuid(),
+      username: faker.internet.username(), 
+      email: faker.internet.email(),
+      password: faker.internet.password(),
+    };
+  }
+
+//   console.log(getRandomUser());
