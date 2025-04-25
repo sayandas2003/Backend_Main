7ad0f8fe1101887e709ac3684ef47fdd32ee77a3
Sayan Das <121926159+sayandas2003@users.noreply.github.com>
2025-04-25 12:00:13 +0530

    faker

--- SQL/index.js
+++ SQL/index.js
@@ -10,24 +10,65 @@ const connection = mysql.createConnection({
   });
 
 
+// Inserting New Data using placeholders (?)
+// let q = "INSERT INTO user (id, username, email, password) VALUES (?, ?, ?, ?)";
+// let user = ["123", "123_newuser", "abc@gmail.com", "abc"];
+
+// try {
+//   connection.query(q, user, (err, result) => {
+//       if(err) throw err;
+//       console.log(result);
+//   });
+// } catch (err) {
+//   console.log(err);
+// }
+
+
+// // for multiple users -
+// let q = "INSERT INTO user (id, username, email, password) VALUES ?";
+// let users = [
+//     ["123b", "123_newuserb", "abc@gmail.comb", "abcb"],
+//     ["123c", "123_newuserc", "abc@gmail.comc", "abcc"]
+// ];
+
+// try {
+//   connection.query(q, [users], (err, result) => {
+//       if(err) throw err;
+//       console.log(result);
+//   });
+// } catch (err) {
+//   console.log(err);
+// }
+
+
+
+// for inserting in bulk using faker -
+let getRandomUser = () => {
+  return [
+      faker.string.uuid(),
+      faker.internet.username(), 
+      faker.internet.email(),
+      faker.internet.password(),
+  ];
+}
+
+let q = "INSERT INTO user (id, username, email, password) VALUES ?";
+let data = [];
+for (let i=1; i<=100; i++) {
+  data.push(getRandomUser()); // 100 fake users data generated and stored
+}
+
 try {
-    connection.query("SHOW TABLES", (err, result) => {
-        if(err) throw err;
-        console.log(result);
-    });
+  connection.query(q, [data], (err, result) => {
+      if(err) throw err;
+      console.log(result);
+  });
 } catch (err) {
-    console.log(err);
+  console.log(err);
 }
 
 
 
-let getRandomUser = () => {
-    return {
-      Id: faker.string.uuid(),
-      username: faker.internet.username(), 
-      email: faker.internet.email(),
-      password: faker.internet.password(),
-    };
-  }
-
 //   console.log(getRandomUser());
+
+connection.end();
