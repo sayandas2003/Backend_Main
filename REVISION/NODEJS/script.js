f67ba35bb1feddaf0a9ac65d84333c064831589d
Sayan Das <121926159+sayandas2003@users.noreply.github.com>
2025-08-21 09:09:45 +0530

    Revision started

--- REVISION/NODEJS/script.js
+++ REVISION/NODEJS/script.js
@@ -1,1 +1,10 @@
 console.log(process.argv);
+const math = require("./math");
+console.log(math(2,3));
+
+console.log(require("./fruits"));
+
+
+const figlet = require("figlet");
+
+await console.log(figlet("sayan"));
