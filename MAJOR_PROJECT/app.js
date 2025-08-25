4d3d8e09f3253992497da0cab6e0d1f5de83bbf7
Sayan Das <121926159+sayandas2003@users.noreply.github.com>
2025-08-25 09:05:38 +0530

    major project initialized

--- MAJOR_PROJECT/app.js
+++ MAJOR_PROJECT/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const Listing = require("./models/listing.js");
 
 
 main()
@@ -13,9 +14,29 @@ main()
 async function main() {
     await mongoose.connect("mongodb://127.0.0.1:27017/wanderlust");
 }
+
+
+
 app.get("/", (req, res) => {
     res.send("Hi, I am root");
 })
+
+app.get("/testListing", (req, res) => {
+    let sampleListing = new Listing({
+        title: "My New Villa",
+        description: "By the lake",
+        price: 1200,
+        location: "Calangute, Goa",
+        country: "India",
+    });
+
+    sampleListing.save()
+    .then( (resp) => {
+        console.log(resp);
+        res.send("successful testing");
+    })
+});
+
 
 app.listen(8080, () => {
     console.log("server is listening to port 8080");
--- MAJOR_PROJECT/models/listing.js
+++ MAJOR_PROJECT/models/listing.js
@@ -2,12 +2,23 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const listingSchema = new Schema({
-    title: String,
+    title: {
+        type: String,
+        required: true,
+    },
     description: String,
-    image: String,
+    image: {
+        filename: String,
+        url: {
+            type: String,
+        default: "https://images.unsplash.com/photo-1500485035595-cbe6f645feb1?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        set: (v) => v === "" ? "https://images.unsplash.com/photo-1500485035595-cbe6f645feb1?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" : v,
+        },
+    },
     price: Number,
     location: String,
     country: String,
 });
 
-const Listing = mongoose.model("Listing")
+const Listing = mongoose.model("Listing", listingSchema);
+module.exports = Listing;
