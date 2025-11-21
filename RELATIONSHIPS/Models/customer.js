b3c38611263863ac62f3228b0d4a91170e05ca85
Sayan Das <121926159+sayandas2003@users.noreply.github.com>
2025-11-21 11:20:48 +0530

    handeled realtionships (one to many) and learnt how to handle deletions in one to many dbs

---
+++ RELATIONSHIPS/Models/customer.js
@@ -0,0 +1,121 @@
+const mongoose = require('mongoose');
+const {Schema} = mongoose;
+
+main()
+    .then(() => {
+        console.log("connection successful");
+    })
+    .catch(err => {
+        console.log(err);
+    });
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/relationDemo');
+}
+
+const orderSchema = new Schema({
+    item: String,
+    price: Number,
+});
+
+const customerSchema = new Schema({
+    name: String,
+    orders: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "Order",
+        },
+    ],
+});
+
+
+// customerSchema.pre("findOneAndDelete", async() => {
+//     console.log("PRE MIDDLEWARE");
+// });
+
+customerSchema.post("findOneAndDelete", async(customer) => {
+    if(customer.orders.length) {
+        let res = await Order.deleteMany({ _id: { $in: customer.orders } });
+        console.log(res);
+    }
+});
+
+
+const Order = mongoose.model("Order", orderSchema);
+const Customer = mongoose.model("Customer", customerSchema);
+
+const addCustomer = async() => {
+    let cust1 = new Customer({
+        name: "Rahul Kumar",
+    });
+    let order1 = await Order.findOne({item: "Chips"});
+    let order2 = await Order.findOne({item: "Chocolate"});
+
+    cust1.orders.push(order1);
+    cust1.orders.push(order2);
+
+    let result = await cust1.save();
+    console.log(result);
+}
+
+// addCustomer();
+
+
+const findCustomer = async() => {
+    let result = await Customer.find({}).populate("orders");
+    console.log(result[0]);
+}
+
+// findCustomer();
+
+// const addOrders = async() => {
+//     let res = await Order.insertMany([
+//         { item: "Samosa", price: 12 },
+//         { item: "Chips", price: 10},
+//         { item: "Chocolate", price: 40}
+//     ]);
+//     console.log(res);
+// };
+
+// addOrders();
+
+
+const deleteCustomers = async () => {
+    let res = await Customer.deleteMany({});
+    console.log("Deleted:", res);
+};
+
+// deleteCustomers();
+
+
+
+
+
+const addCust = async() => {
+    let newCust = new Customer({
+        name: "Karan Arjun"
+    });
+
+    let newOrder = new Order({
+        item: "Burger",
+        price: 250
+    });
+
+    newCust.orders.push(newOrder);
+
+    await newOrder.save();
+    await newCust.save();
+
+    console.log("added new customer");
+};
+
+
+const delCust = async() => {
+    let data = await Customer.findByIdAndDelete("691e9ced8f88c906445dd7b4");
+    console.log(data);
+}
+
+delCust();
+
+
+// addCust();
---
+++ RELATIONSHIPS/Models/posts.js
@@ -0,0 +1,71 @@
+const mongoose = require('mongoose');
+const {Schema} = mongoose;
+
+main()
+    .then(() => {
+        console.log("connection successful");
+    })
+    .catch(err => {
+        console.log(err);
+    });
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/relationDemo');
+}
+
+
+const userSchema = new Schema ({
+    username: String,
+    email: String
+});
+
+const postSchema = new Schema({
+    content: String,
+    likes: Number,
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: "User"
+    }
+});
+
+const User = mongoose.model("User", userSchema);
+const Post = mongoose.model("Post", postSchema);
+
+const addData = async() => {
+    let user1 = new User({
+        username: "rahulkumar",
+        email: "rahul@gmail.com"
+    });
+
+    let post1 = new Post({
+        content: "hello world!",
+        likes: 7
+    });
+    post1.user = user1;
+
+    await user1.save();
+    await post1.save();
+};
+
+// addData();
+
+
+const deleteCustomers = async () => {
+    let res = await User.deleteMany({});
+    let ress = await Post.deleteMany({});
+    console.log("Deleted:", res, ress);
+};
+
+// deleteCustomers();
+
+
+
+const getData = async() => {
+    let result = await Post.findOne({}).populate("user", "username -_id");
+    console.log(result);
+};
+
+getData();
+
+
+
